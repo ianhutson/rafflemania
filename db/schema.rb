b99cba9111c4f28be17564163ee3459bc3deccdc
ianhutson <ih00209@georgiasouthern.edu>
2020-10-07 18:13:01 -0400

    switched from sqlite to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_03_215443) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "raffles", force: :cascade do |t|
     t.string "product_name"
     t.string "product_description"
