4a5cc094b1ba894d3a9ac611d19e76884a7bb3e5
ianhutson <ih00209@georgiasouthern.edu>
2020-10-12 16:50:48 -0400

    added nested resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :raffles, :path_names => {:edit => 'enter' }
   resources :tickets, :path_names => {:edit => 'claim' }
-  resources :users do
-    get "history"
+  resources :raffles do
+    resources :users, only: [:show]
   end
-
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   root 'welcome#home'
   get '/sign_up', to: 'users#new'
